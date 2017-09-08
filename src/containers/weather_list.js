748ffd2dbb19534fa631b5a2538fdb007d06b6d7
zezemanolo <manoloish@gmail.com>
2017-09-08 11:15:02 -0700

    adds react-sparklines and can now render weather data in linear graph

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		const cityName = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+
 		return (
 			<tr key={cityName}>
 				<td>{cityName}</td>
+				<td>
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+					<Sparklines height={120} width={180} data={pressures}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+					<Sparklines height={120} width={180} data={humidities}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+				</td>
 			</tr>
 		);
 	}
