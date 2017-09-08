5949e0e333883e91e767b0846ca451e31cffd579
zezemanolo <manoloish@gmail.com>
2017-09-08 08:59:58 -0700

    binds inputchange function, adds css classes to form and child elements

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+
+		// binding onInputChange to this (which is SearchBar)
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({ term: event.target.value });
+	}
+
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input
+					placeholder="Get a five-day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">
 						Submit
