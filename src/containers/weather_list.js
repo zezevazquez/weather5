3aea86a10dc258d53c14451e95de1194f1f07aad
zezemanolo <manoloish@gmail.com>
2017-09-08 11:37:03 -0700

    adds react-sparklines and separates charts to their own component. Returns averages numbers and average line for each chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
@@ -13,15 +13,13 @@ class WeatherList extends Component {
 			<tr key={cityName}>
 				<td>{cityName}</td>
 				<td>
-					<Sparklines height={120} width={180} data={temps}>
-						<SparklinesLine color="red" />
-					</Sparklines>
-					<Sparklines height={120} width={180} data={pressures}>
-						<SparklinesLine color="red" />
-					</Sparklines>
-					<Sparklines height={120} width={180} data={humidities}>
-						<SparklinesLine color="red" />
-					</Sparklines>
+					<Chart data={temps} color="peru" units="K" />
+				</td>
+				<td>
+					<Chart data={temps} color="red" units="hPa" />
+				</td>
+				<td>
+					<Chart data={pressures} color="limegreen" units="%" />
 				</td>
 			</tr>
 		);
@@ -32,9 +30,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>{this.props.weather.map(this.renderWeather)}</tbody>
